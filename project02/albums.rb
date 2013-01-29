e6d9bee9a78545896df012d34208a1a988742240
Ricardo Lopez <rlopez@mymail.mines.edu>
2013-01-28 22:53:44 -0700

    Adding the rack app, .erb templates, css stylesheet. Hooray for procrastination

--- project02/albums.rb
+++
@@ -1,9 +0,0 @@
-require 'rack'
-
-class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
-  end
-end
-
-Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
