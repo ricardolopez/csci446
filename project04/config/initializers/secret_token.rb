092859b0d9b6bb52391ba155825bc8d2f8a981dd
Ricardo Lopez <rlopez@mymail.mines.edu>
2013-03-05 20:47:09 -0700

    Adding /images /stylesheets folders, added a few seeds, changed the views.  I am now complete with chapter 6 of the Rails tutorial

--- project04/config/initializers/secret_token.rb
+++ project04/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '47e0993da52ba9eb940634ff4d713cab611e09c15ecb3d942b32b974e2c95ab1e99726de24a3031409ab32d61650644f64a2451b054c0b40115fc60f9b0258fc'
+Depot::Application.config.secret_token = 'e5fb4fcef09c18a4172f68f2e4c7a3f4fb88d894a3fff6f340243cd43e036c62eb214b58633df4319770c14a4e415c46803baeede75dbb659c743bf98b31ed70'
